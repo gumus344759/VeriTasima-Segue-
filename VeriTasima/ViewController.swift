2e9c2750dc8537341c8de5f9e2ac8ddcd3ec14d3
MacBook <macbook@macbooks-macbook-pro.local>
2016-02-28 13:24:38 +0200

    Initial Commit

---
+++ VeriTasima/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VeriTasima
+//
+//  Created by MacBook on 28/02/16.
+//  Copyright © 2016 sgumus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ VeriTasimaTests/VeriTasimaTests.swift
@@ -0,0 +1,36 @@
+//
+//  VeriTasimaTests.swift
+//  VeriTasimaTests
+//
+//  Created by MacBook on 28/02/16.
+//  Copyright © 2016 sgumus. All rights reserved.
+//
+
+import XCTest
+@testable import VeriTasima
+
+class VeriTasimaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
